5570f26c77c42bc61acfb542b445d115381fbed3
François D <fderu44@gmail.com>
2023-07-03 10:10:14 +0200

    Ajout de la fonction changeColor()

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const grid = document.getElementById('grid');
 let size = 16;
 
+function changeColor() {
+    this.style.backgroundColor = "orange";
+}
+
 function buildGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -10,6 +14,9 @@ function buildGrid(size) {
         cell.classList.add('cell');
         grid.appendChild(cell);
     }
+
+    let gridPixels = document.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener("mouseover", changeColor));
 }
 
 buildGrid(size);
